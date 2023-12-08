93065ff92e86bf4ce71664eacc5f32c25e0537cf
Dima <derferth@yandex.ru>
2023-12-08 21:02:27 +0500

    Структура человека с не изменяемыми характеристиками

---
+++ Huma'n.h
@@ -0,0 +1,48 @@
+
+
+#ifndef PERSON_S_AGE_HUMA_N_H
+#define PERSON_S_AGE_HUMA_N_H
+
+#include "iostream"
+#include "string"
+
+
+using namespace std;
+
+struct Human {
+private:
+    string surname;
+    string name;
+    string patronymic;
+public:
+    short age = 0;
+
+    Human(string surname_duplicate, string name_duplicate, string patronymic_duplicate) {
+        surname = surname_duplicate;
+        name = name_duplicate;
+        patronymic = patronymic_duplicate;
+    }
+
+
+    string GetSurname() {
+        return surname;
+    }
+
+    string GetName() {
+        return name;
+    }
+
+    string GetPatronymic() {
+        return patronymic;
+    }
+
+    string Print() {
+        return GetSurname() + " " + GetName() + " " + GetPatronymic() + " " +
+                to_string(age) + "\n";
+    }
+
+    ~Human() = default;
+};
+
+
+#endif //PERSON_S_AGE_HUMA_N_H
---
+++ main.cpp
@@ -0,0 +1,60 @@
+//Задание.
+//Создать структуру Человек с хранением ФИО и возраста.
+// Предусмотреть невозможность редактирования ФИО.
+// Предусмотреть возможность изменения возраста
+// с проверкой на отрицательное и слишком большое (более 170) значение.
+
+
+
+#include <iostream>
+#include "Huma'n.h"
+
+
+short ChengAge();
+
+Human AddPerson();
+
+int main() {
+    bool flag;
+    auto *person1 = new Human(AddPerson());
+    person1->age = ChengAge();
+    cout << person1->Print();
+
+    cout << " Do you want chang age?:\n 1 - yes\n 0 - no";
+    cin >> flag;
+    if (flag) {
+        person1->age = ChengAge();
+    }
+    cout << person1->Print();
+
+    delete person1;
+
+    return 0;
+}
+
+Human AddPerson() {
+
+    cout << "Enter surname: \n";
+    string surname;
+    cin >> surname;
+
+    cout << "Enter  name : \n";
+    string name;
+    cin >> name;
+
+    cout << "Enter patronymic : \n";
+    string patronymic;
+    cin >> patronymic;
+
+    return {surname, name, patronymic};
+}
+
+short ChengAge() {
+    cout << " Enter age: \n";
+    short age;
+    cin >> age;
+    if (age < 0 || age > 170) {
+        cout << "Error";
+        return 0;
+    } else return age;
+}
